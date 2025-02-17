9265d19bc91d2d867b5289b3665275f71a3468d5
Abhay Chaurasia <ac37636@gmail.com>
2025-02-17 20:34:22 +0530

    FirstCommit with Code

---
+++ DataStructure_WithSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataStructure_WithSwift
+//
+//  Created by Abhay Chaurasia on 17/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("heloo")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DataStructure_WithSwiftUITests/DataStructure_WithSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DataStructure_WithSwiftUITestsLaunchTests.swift
+//  DataStructure_WithSwiftUITests
+//
+//  Created by Abhay Chaurasia on 17/02/25.
+//
+
+import XCTest
+
+final class DataStructure_WithSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
