ee2207762e1e85ccaf2c230f7a2b73d2190fee25
Abhay Chaurasia <ac37636@gmail.com>
2025-03-06 00:49:48 +0530

    positiveNegativeNum

---
+++ DataStructure_WithSwift/Array/insert duplicate of K adjacent /InsertDuplicate.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+var greeting = "Hello, playground"
+/*
+ Insert duplicate of K adjacent to it for it’s every occurrence in array
+  Given an array arr consisting of N integers and an integer K, the task is to insert an adjacent K for every occurrence of it in the original sequence and then truncate the array to the original length using an O(1) auxiliary space.
+
+ Examples:
+
+
+ Input: arr[] = {1, 0, 2, 3, 0, 4, 5, 0}, K = 0
+ Output: {1, 0, 0, 2, 3, 0, 0, 4}
+ Explanation:
+ The given array {1, 0, 2, 3, 0, 4, 5, 0} is modified to {1, 0, 0, 2, 3, 0, 0, 4] after insertion of two 0’s and truncation of extra elements.
+
+
+ Input: arr[] = {7, 5, 8}, K = 8
+ Output: {7, 5, 8}
+ Explanation:
+ After inserting an adjacent 8 into the array, it got truncated to restore the original size of the array.
+
+ */
+struct InsertDuplicate {
+    
+    func  arrangeValue(_ nums : [Int] , K : Int) -> [Int] {
+        var n =  nums.count
+        for i  in 0..<nums.count {
+            if nums[i] == k  && i < n-1{
+                var temp = nums[i + 1]
+                nums[i + 1] = i
+                nums[i + 2]  = temp
+                nums.remove(at: n - 1)
+            }
+            
+        }
+        
+        return nums
+    }
+    
+    
+    
+}
+
+var value = [1, 0, 2, 3, 0, 4, 5, 0]
+var getarry = InsertDuplicate().arrangeValue(value, K: 0)
+print(getarry)
